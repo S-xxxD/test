7158ccc47fbf2fa49d4a3970c4b14fc038f9ed86
songxiaodong <1265215412@qq.com>
2024-10-16 20:07:20 +0800

    -'Test1'

---
+++ src/main/java/com/xxd/textmaven/App.java
@@ -0,0 +1,13 @@
+package com.xxd.textmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
